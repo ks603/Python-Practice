c30a9e9abb4de39e0b0e797476ce3a7165d5bfd0
ks603 <kevin.shaughnessy18@gmail.com>
2021-06-27 18:01:52 -0400

    Practice with conditionals

--- Practice.py
+++ Practice.py
@@ -11,10 +11,12 @@ else:
     print('wrong password')
 
 name = 'Bob'
-age = 3000
+age = 200
 if name == 'Alice':
     print('Hello Alice')
 elif age < 12:
-    print('You are not Alice')
+    print('You are not Human')
 elif age > 2000:
     print('Not a human')
+elif age > 100:
+    print('test')
