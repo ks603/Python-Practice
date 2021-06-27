dc63dcbaeaa7a3fd31a80ad990252d1cc2e441b1
ks603 <kevin.shaughnessy18@gmail.com>
2021-06-27 17:46:58 -0400

    Python practice

--- newproj.py
+++ newproj.py
@@ -11,6 +11,27 @@ myAge = input()
 print('You will be ' + str(int(myAge) + 1) + ' in a year')
 
 
-print('What is your age?')  # ask for their age
-myAge = input()
-print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+print(42 == 42)
+print(42 == 'hello')
+print(42 == 41)
+print(2 != 3)
+print(42 < 100)
+print(42 >= 100)
+print(42 < 42)
+print(42 <= 42)
+myAge = 26
+print(myAge < 30)
+print(42 == '42')
+print(42.0 == 42)
+print(True and True)
+print(True and False)
+print(False and False)
+print(True or True)
+print(True or False)
+print(False or False)
+print(not True)
+print(not False)
+
+myAge = 26
+myPet = 'cat'
+print(myAge > 20 and myPet == 'cat')
