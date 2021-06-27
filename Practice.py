443c390874674de2dbaeadbe6e30c8f114090cb7
ks603 <kevin.shaughnessy18@gmail.com>
2021-06-27 18:00:34 -0400

    Practice with condtionals

---
+++ Practice.py
@@ -0,0 +1,20 @@
+name = 'Alice'
+if name == 'Alice':
+    print('Hi Alice')
+
+print('Done')
+
+password = 'swordfish'
+if password == 'swordfish':
+    print('Access gratned.')
+else:
+    print('wrong password')
+
+name = 'Bob'
+age = 3000
+if name == 'Alice':
+    print('Hello Alice')
+elif age < 12:
+    print('You are not Alice')
+elif age > 2000:
+    print('Not a human')
