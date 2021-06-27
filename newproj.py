1b86b2e4d6a4cb9cfbfc2ac1d9b1e5dc85c73740
ks603 <kevin.shaughnessy18@gmail.com>
2021-06-27 16:38:18 -0400

    Test commit

--- newproj.py
+++ newproj.py
@@ -3,9 +3,14 @@
 print('Hello World')
 print('What is your name')  # ask for their name
 myName = input()
-print('IT is good to mee you ' + myName)
+print('It is good to mee you ' + myName)
 print('The length of you name is:')
 print(len(myName))
 print('What is your age?')  # ask for their age
 myAge = input()
 print('You will be ' + str(int(myAge) + 1) + ' in a year')
+
+
+print('What is your age?')  # ask for their age
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
